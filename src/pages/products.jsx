69a0d66ef65e160e25fde0b83ae6cce3cd3ad9ef
adeard <adeardian1994@gmail.com>
2023-08-31 14:03:33 +0700

    finalize tutorial 14

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CardProduct from '../components/Fragments/CardProduct'
 import Button from '../components/Elements/Button/Index';
 import Counter from '../components/Fragments/Counter';
@@ -33,6 +33,23 @@ const email = localStorage.getItem('email')
 const ProductsPage = () => {
 
     const [cart, setCart] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
+    
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem("cart")) || [])
+    }, []);
+
+    useEffect(() => {
+        if (cart.length > 0) {
+            const sum = cart.reduce((acc, item) => {
+                const product = products.find((product) => product.id === item.id)
+                return acc + product.price * item.qty
+            }, 0)
+            setTotalPrice(sum)
+            localStorage.setItem("cart", JSON.stringify(cart))
+        }
+    }, [cart])
+
     const handleLogout = () => {
         localStorage.removeItem('email')
         localStorage.removeItem('password')
@@ -99,14 +116,27 @@ const ProductsPage = () => {
                                     })}</td>
                                 </tr>
                             );
-                        })}                        
+                        })}
+                        <tr>
+                            <td colSpan={3}>
+                                <b>Total Price</b>
+                            </td>
+                            <td>
+                                <b>
+                                    Rp. {(totalPrice).toLocaleString("id-ID", {
+                                        styles : "currency",
+                                        currency : "IDR",
+                                    })}
+                                </b>
+                            </td>
+                        </tr>                        
                     </tbody>
                 </table>
             </div>
         </div>
-        <div className='flex w-100 justify-center'>
+        {/* <div className='flex w-100 justify-center'>
             <Counter></Counter>
-        </div>
+        </div> */}
     </>
   )
 }
