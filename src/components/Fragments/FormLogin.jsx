60bbaa1d1f320915579b4d603a0a127396dee243
adeard <adeardian1994@gmail.com>
2023-09-03 00:35:24 +0700

    finalize tutorial 17

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,29 +1,42 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { InputForm } from '../Elements/Input/Index'
 import Button from '../Elements/Button/Index'
+import { login } from '../../services/auth.service'
 
 const FormLogin = () => {
+  const [loginFailed, setLoginFailed] = useState("")
 
   const handleLogin = (event) => {
     event.preventDefault()
-    localStorage.setItem('email', event.target.email.value)
-    localStorage.setItem('password', event.target.password.value)
-    console.log(localStorage.getItem('email'))
-    console.log(localStorage.getItem('password'))
-    window.location.href = "/products"
+    // localStorage.setItem('email', event.target.email.value)
+    // localStorage.setItem('password', event.target.password.value)
+    // window.location.href = "/products"
+    const data = {
+      username: event.target.username.value,
+      password: event.target.password.value
+    }
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem('token', res)
+        window.location.href = "/products"
+      } else {
+        setLoginFailed(res.response.data)
+      }
+    })
   }
 
-  const emailRef = useRef(null)
+  const usernameRef = useRef(null)
 
   useEffect(() => {
-    emailRef.current.focus()
+    usernameRef.current.focus()
   }, [])
   return (
-    <form onSubmit={handleLogin}>
-        <InputForm label="Email" type="email" name="email" placeholder="example@mail.com" ref={emailRef} />
-        <InputForm label="Password" type="password" name="password" placeholder="*****" />
-        <Button classname="bg-blue-600 w-full" type="submit">Login</Button>
-    </form>
+      <form onSubmit={handleLogin}>
+      {loginFailed && <p className='text-red-600'>{loginFailed}</p>}      
+          <InputForm label="Username" type="text" name="username" placeholder="username" ref={usernameRef} />
+          <InputForm label="Password" type="password" name="password" placeholder="*****" />
+          <Button classname="bg-blue-600 w-full" type="submit">Login</Button>
+      </form>
   )
 }
 
